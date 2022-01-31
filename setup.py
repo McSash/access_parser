b18a3bca4b07b6308be9cf4e1121e1eea1a732db
ZOSSAUME <sascha.saumer@zeiss.com>
2022-01-31 14:47:53 +0100

    setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as f:
 
 setuptools.setup(
     name="access_parser_c",
-    version="0.0.5",
+    version="0.0.6",
     author="Sascha Saumer",
     author_email="s.saumer@hotmail.de",
     description="Access database (*.mdb, *.accdb) parser",
